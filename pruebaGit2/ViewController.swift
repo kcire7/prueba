92e58224b3210c7b6fbc0b214babfd4d451d46ca
Erick Rodríguez Ramos <erick@macbook-pro-de-erick.local>
2016-09-14 20:46:34 -0500

    Initial Commit

---
+++ pruebaGit2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit2
+//
+//  Created by Erick Rodríguez Ramos on 14/09/16.
+//  Copyright © 2016 Erick Rodríguez Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
